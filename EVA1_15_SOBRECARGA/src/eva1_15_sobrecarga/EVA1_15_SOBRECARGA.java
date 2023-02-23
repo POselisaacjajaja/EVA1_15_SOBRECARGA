28f41a8d6f7dcea5b70d3434d1df6e27974b0563
POselisaacjajaja <124391806+POselisaacjajaja@users.noreply.github.com>
2023-02-23 12:51:18 -0600

    Add files via upload

---
+++ EVA1_15_SOBRECARGA/src/eva1_15_sobrecarga/EVA1_15_SOBRECARGA.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_15_sobrecarga;
+
+/**
+ *
+ * @author isaaczapatto
+ */
+public class EVA1_15_SOBRECARGA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println(100);
+        System.out.println(true);
+        System.out.println(args);
+        System.out.println(10);
+    }
+    public static void algo(){
+        
+    }
+    public static void algo(int y){
+        
+        
+        
+    }
+}
